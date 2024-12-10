585df383472313e942a857207540251d2596cb53
Dmitriy Kasimov <its.dmitriy.kasimov@gmail.com>
2024-12-10 19:38:41 +0300

    extended ui-kit: added <Skeleton />

--- src/entities/Ticket/ui/components/TicketPriceInfo/TicketPriceInfo.tsx
+++ src/entities/Ticket/ui/components/TicketPriceInfo/TicketPriceInfo.tsx
@@ -5,6 +5,7 @@ import { AppImage } from '@/shared/ui-kit/AppImage'
 import { Button } from '@/shared/ui-kit/Button'
 import { Text } from '@/shared/ui-kit/Text'
 import { TicketType } from '@/entities/Ticket/model/types/TicketProps.ts'
+import { Skeleton } from '@/shared/ui-kit/Skeleton'
 
 type TicketPriceInfoProps = {
     ticket: TicketType
@@ -12,7 +13,11 @@ type TicketPriceInfoProps = {
 export const TicketPriceInfo: FC<TicketPriceInfoProps> = ({ ticket }) => {
     return (
         <VStack className={cls.PriceBlock} align={'center'} gap={'8'}>
-            <AppImage fallback={<>Loading...</>} src={ticket.linkCompanyLogo} className={cls.img} />
+            <AppImage
+                fallback={<Skeleton width={184} height={80} border={'8px'} />}
+                src={ticket.linkCompanyLogo}
+                className={cls.img}
+            />
 
             <Button fillVariant={'primary'}>
                 <Text color={'main-inverted'} noWrap>
