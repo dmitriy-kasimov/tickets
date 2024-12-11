ba6984a7d0fa892f9c6be7b8f64c4d78f6a4f6bf
Dmitriy Kasimov <its.dmitriy.kasimov@gmail.com>
2024-12-11 22:11:47 +0300

    add new feature <ScrollToTopButton />

--- src/shared/layouts/MainLayout/ui/MainLayout.tsx
+++ src/shared/layouts/MainLayout/ui/MainLayout.tsx
@@ -4,12 +4,16 @@ import { MainLayoutProps } from '@/shared/layouts/MainLayout/types/MainLayoutPro
 import cls from './MainLayout.module.scss'
 
 import { VStack } from '@/shared/ui-kit/Stack'
+import { ScrollToTopButton } from '@/features/scrollToTopButton'
 
 export const MainLayout: FC<MainLayoutProps> = memo(({ header, content }) => {
     return (
         <VStack className={cls.MainLayout} gap={'16'} max align={'center'}>
             <div className={cls.header}>{header}</div>
             <div className={cls.content}>{content}</div>
+            <div className={cls.toolBar}>
+                <ScrollToTopButton />
+            </div>
         </VStack>
     )
 })
