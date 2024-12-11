8d51345b6119f8f9dcf4dca961d21ea36ac749fc
Dmitriy Kasimov <its.dmitriy.kasimov@gmail.com>
2024-12-11 18:19:51 +0300

    linked Filter with search params (installed react-router-dom)

--- src/features/FilterOfTickets/ui/FilterOfTickets.tsx
+++ src/features/FilterOfTickets/ui/FilterOfTickets.tsx
@@ -1,12 +1,26 @@
-import { FC, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { VStack } from '@/shared/ui-kit/Stack'
 import { Text } from '@/shared/ui-kit/Text'
-import { CurrencySelector, CurrencyType } from '@/features/CurrencySelector'
+import { CurrencySelector, CurrencyType } from '@/entities/CurrencySelector'
 import { TransfersBlock } from '@/features/FilterOfTickets/ui/TransfersBlock.tsx'
+import { createSearchParams, useLocation, useNavigate } from 'react-router-dom'
 
 export const FilterOfTickets: FC = () => {
     const [currency, setCurrency] = useState<CurrencyType>('rub')
     const [transfers, setTransfers] = useState<boolean[]>(new Array(4).fill(false))
+
+    const { pathname } = useLocation()
+    const navigate = useNavigate()
+    useEffect(() => {
+        navigate({
+            pathname,
+            search: createSearchParams({
+                currency,
+                transfers: `${transfers.map(option => +option)}`
+            }).toString()
+        })
+    }, [currency, transfers])
+
     return (
         <VStack gap={'16'}>
             <VStack gap={'8'}>
